3f26bf91620c9e975fc0aaba3415e6de9fe037d2
kadelcode <kadelcode@gmail.com>
2025-07-12 21:28:58 +0100

    Prettier code formatting fix

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
-import { ReactNode } from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { queryClient } from '@/lib/react-query';
+import { ReactNode } from "react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { queryClient } from "@/lib/react-query";
 
 // Providers component to wrap the application with React Query context
 export default function Providers({ children }: { children: ReactNode }) {
--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,11 +1,11 @@
 import { QueryClient } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            retry: 1,
-            refetchOnWindowFocus: false,
-            staleTime: 100 * 60 * 5, // 5 minutes
-        }
-    }
-})
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+      staleTime: 100 * 60 * 5, // 5 minutes
+    },
+  },
+});
