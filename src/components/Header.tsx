1fb5ea85e3acc3719171dd93e45386babe376cda
kadelcode <kadelcode@gmail.com>
2025-07-14 07:09:42 +0100

    Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+
+export default function Header() {
+    return (
+        <header className='bg-white border-b shadow-sm p-4 mb-6'>
+            <nav className='flex justify-between items-center container mx-auto'>
+                <Link href="/" className='text-xl font-bold'>Mini-Commerce</Link>
+                <div className='space-x-4'>
+                    <Link href="/cart">Cart</Link>
+                    <Link href="/checkout">Checkout</Link>
+                </div>
+            </nav>
+        </header>
+    )
+}
